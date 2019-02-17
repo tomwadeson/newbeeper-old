9c86612533ab0f45d8edf79d2a760c3a85abf05e
Tom Wadeson <tom@tomwadeson.com>
2019-02-17 17:38:36 +0000

    Remove duplication

--- api/src/test/scala/newbeeper/api/InstructorServiceSpec.scala
+++ api/src/test/scala/newbeeper/api/InstructorServiceSpec.scala
@@ -19,15 +19,12 @@ import org.scalatest.prop.PropertyChecks
 class InstructorServiceSpec extends FreeSpec with PropertyChecks with Codecs {
 
   "POST to /instructors should create an instructor" in {
-
-    val request = Request[IO](method = Method.POST)
-
     forAll { instructorForm: InstructorForm =>
       val (response, created) =
         withClient { client =>
           for {
-            (response, created) <- client.fetch(request.withEntity(instructorForm.asJson))(res =>
-                                    res.as[Instructor[Instructor.Id]].tupleLeft(res))
+            (response, created) <- client.fetch(createRequest.withEntity(instructorForm.asJson))(
+                                    res => res.as[Instructor[Instructor.Id]].tupleLeft(res))
           } yield (response, created)
         }.unsafeRunSync()
 
@@ -37,9 +34,6 @@ class InstructorServiceSpec extends FreeSpec with PropertyChecks with Codecs {
   }
 
   "GET to /instructors should retrieve all instructors" in {
-
-    val createInstructorReq = Request[IO](method = Method.POST)
-
     forAll { instructorForms: List[InstructorForm] =>
       val (created, retrieved) =
         withClient { client =>
@@ -48,7 +42,7 @@ class InstructorServiceSpec extends FreeSpec with PropertyChecks with Codecs {
                                  .traverse(
                                    i =>
                                      client
-                                       .fetch(createInstructorReq.withEntity(i.asJson))(res =>
+                                       .fetch(createRequest.withEntity(i.asJson))(res =>
                                          res.as[Instructor[Instructor.Id]])
                                        .attempt)
             created = creationAttempts collect { case Right(i) => i }
@@ -63,14 +57,12 @@ class InstructorServiceSpec extends FreeSpec with PropertyChecks with Codecs {
   "GET to /instructors/$id" - {
 
     "should retrieve the instructor when they exist" in {
-      val createInstructorReq = Request[IO](method = Method.POST)
-
       forAll { instructorForm: InstructorForm =>
         val (created, retrieved) =
           withClient { client =>
             for {
               created <- client.fetchAs[Instructor[Instructor.Id]](
-                          createInstructorReq.withEntity(instructorForm.asJson))
+                          createRequest.withEntity(instructorForm.asJson))
               retrieved <- client.expect[Instructor[Instructor.Id]](s"/${created.id.value}")
             } yield (created, retrieved)
           }.unsafeRunSync()
@@ -93,19 +85,15 @@ class InstructorServiceSpec extends FreeSpec with PropertyChecks with Codecs {
 
   "PATCH to /instructors/$id" - {
 
-    val createInstructorReq = Request[IO](method = Method.POST)
-    val patchInstructorReq = Request[IO](method = Method.PATCH)
-
     "should update the instructor when they exist" in {
-
       forAll { (instructorForm: InstructorForm, patchForm: InstructorPatch) =>
         val (created, updated, retrieved) =
           withClient { client =>
             for {
               created <- client.fetchAs[Instructor[Instructor.Id]](
-                          createInstructorReq.withEntity(instructorForm.asJson))
+                          createRequest.withEntity(instructorForm.asJson))
               updated <- client.fetchAs[Instructor[Instructor.Id]](
-                          patchInstructorReq
+                          patchRequest
                             .withUri(Uri.unsafeFromString(s"/${created.id.value}"))
                             .withEntity(patchForm.asJson))
               retrieved <- client.expect[Instructor[Instructor.Id]](s"/${created.id.value}")
@@ -125,7 +113,7 @@ class InstructorServiceSpec extends FreeSpec with PropertyChecks with Codecs {
           val status =
             withClient { client =>
               client.fetch(
-                patchInstructorReq
+                patchRequest
                   .withUri(Uri.unsafeFromString(s"/$id"))
                   .withEntity(patchForm.asJson))(r => IO.pure(r.status))
             }.unsafeRunSync()
@@ -136,22 +124,19 @@ class InstructorServiceSpec extends FreeSpec with PropertyChecks with Codecs {
   }
 
   "DELETE to /instructors/$id should delete the instructor when they exist" in {
-
-    val getReq = Request[IO]()
-    val createReq = Request[IO](method = Method.POST)
-    val deleteReq = Request[IO](method = Method.DELETE)
-
     forAll { instructorForm: InstructorForm =>
       val (retrievedPreDelete, retrievedPostDelete) =
         withClient { client =>
           for {
             created <- client.fetchAs[Instructor[Instructor.Id]](
-                        createReq.withEntity(instructorForm.asJson))
+                        createRequest.withEntity(instructorForm.asJson))
             retrievedPreDelete <- client.status(
-                                   getReq.withUri(Uri.unsafeFromString(s"/${created.id.value}")))
-            _ <- client.status(deleteReq.withUri(Uri.unsafeFromString(s"/${created.id.value}")))
+                                   getRequest.withUri(
+                                     Uri.unsafeFromString(s"/${created.id.value}")))
+            _ <- client.status(deleteRequest.withUri(Uri.unsafeFromString(s"/${created.id.value}")))
             retrievedPostDelete <- client.status(
-                                    getReq.withUri(Uri.unsafeFromString(s"/${created.id.value}")))
+                                    getRequest.withUri(
+                                      Uri.unsafeFromString(s"/${created.id.value}")))
           } yield (retrievedPreDelete, retrievedPostDelete)
         }.unsafeRunSync()
 
@@ -173,4 +158,10 @@ class InstructorServiceSpec extends FreeSpec with PropertyChecks with Codecs {
 
   implicit val instructorsDecoder: EntityDecoder[IO, List[Instructor[Instructor.Id]]] =
     jsonOf[IO, List[Instructor[Instructor.Id]]]
+
+  private val createRequest = Request[IO](method = Method.POST)
+  private val getRequest = Request[IO]()
+  private val patchRequest = Request[IO](method = Method.PATCH)
+  private val deleteRequest = Request[IO](method = Method.DELETE)
+
 }
